1abaf8446d0a9073df4fb8ca0870921dddc5341b
Jose Luis Llorente <joseluis.llorenteperales@gmail.com>
2016-05-05 00:29:50 +0200

    Importando el proyecto git-demo

---
+++ src/main/java/es/curso/integracioncontinua/GitDemo/App.java
@@ -0,0 +1,13 @@
+package es.curso.integracioncontinua.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
